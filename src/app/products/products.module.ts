1521172b8a8538fc7b36ffeb02324995007bfa04
Justin Lee Attinoto <justinattinoto@gmail.com>
2025-04-27 15:01:08 +0200

    feat: add ProductDetailsComponent and routing for product details view

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductslistComponent } from './productslist/productslist.component';
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    ProductslistComponent
+    ProductslistComponent,
+    ProductdetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class ProductsModule { }
